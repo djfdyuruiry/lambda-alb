7ffb32cff9dcceab005ba43e651597531edc4de5
djfdyuruiry <djfdyuruiry@noreply.github.com>
2020-05-17 03:21:40 +0100

    Fixing issue with docker publishing script

--- scripts/buildAndPublishDocker.sh
+++ scripts/buildAndPublishDocker.sh
@@ -6,22 +6,23 @@ imageName="djfdyuruiry/lambda-alb"
 function publishImage() {
     version="$1"
 
-    echo "Publishing ${imageName} v${version} to Docker Hub..."
+    echo "Publishing ${imageName} ${version} to Docker Hub..."
 
     docker push "${imageName}:latest"
-    docker push "${imageName}:v${version}"
+    docker push "${imageName}:${version}"
 }
 
 function buildAndTagImage() {
     version="$1"
 
-    echo "Building ${imageName} v${version} docker image..."
+    echo "Building ${imageName} ${version} docker image..."
 
     docker build --build-arg "PACKAGE_VERSION=@${version}" --tag "${imageName}" --tag "${imageName}:${version}" .
 }
 
 function buildAndPublish() {
     version=$(node -e "console.log(require('./package.json').version)")
+    version="v${version}"
 
     buildAndTagImage "${version}"
     publishImage "${version}"
